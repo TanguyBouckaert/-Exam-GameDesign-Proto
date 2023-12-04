c2aa1bafed93dce2af37ed6ec9003403d7b77b12
TanguyBouckaert <tanguy.bouckaert@student.howest.be>
2023-12-04 16:56:28 +0100

    Added train movement and reset button

---
+++ Assets/Scripts/TrainController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour
+{
+    float moveSpeed = 5.0f;
+    Vector3 _startPosition;
+    Quaternion _startRotation;
+
+    private void Awake()
+    {
+        _startPosition = transform.position;
+        _startRotation = transform.rotation;
+        Debug.Log($"This is the location of the train: {_startPosition}.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow)) // Map this to a the Right Trigger on a Controller
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Q)) // Map this later to a on the controller
+        { 
+
+            transform.position = _startPosition;
+            transform.rotation = _startRotation;
+            Debug.Log("Reset");
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Your MOM!");
+        transform.rotation = other.transform.rotation;
+    }
+}
